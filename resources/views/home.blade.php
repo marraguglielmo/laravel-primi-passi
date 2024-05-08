19bea4d4c63e1887809c2228142ad72900ce8a7f
Guglielmo Marra <156308467+marraguglielmo@users.noreply.github.com>
2024-05-08 15:26:59 +0200

    [add] home.blade

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- bootstrap --}}
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css'
+        integrity='sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg=='
+        crossorigin='anonymous' />
+    <title>Primi Passi con Laravel</title>
+</head>
+
+<body>
+    <h1>Hello Laravel</h1>
+    <h2>Home</h2>
+</body>
+
+</html>
